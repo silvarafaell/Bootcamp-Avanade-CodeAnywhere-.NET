9b4ad9f782bdae947e5dc3196b16cde48b03b950
Francisco Rafael <francisco_rafael@hotmail.com.br>
2021-08-27 07:39:58 -0300

    Criando a primeira aplicação ASPNET

---
+++ Desenvolvimento de aplicações com .NET/PrimeiraAplicacao/PrimeiraAplicacao/Models/Context.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrimeiraAplicacao.Models
+{
+    public class Context : DbContext
+    {
+        public DbSet<Categoria> Categorias { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString: @"Server=DESKTOP-EGDDQI4\SQLEXPRESS;Database=Cursomvc;User Id=sa;Password=1234;");
+        }
+    }
+}
