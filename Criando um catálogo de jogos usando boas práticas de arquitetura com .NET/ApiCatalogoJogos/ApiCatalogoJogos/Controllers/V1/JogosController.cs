813e0813312507a6b922e798f5311f8ff4d0f668
Francisco Rafael <francisco_rafael@hotmail.com.br>
2021-09-06 09:05:14 -0300

    parte 2

---
+++ Criando um catálogo de jogos usando boas práticas de arquitetura com .NET/ApiCatalogoJogos/ApiCatalogoJogos/Controllers/V1/JogosController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCatalogoJogos.Controllers.V1
+{
+    [Route("api/V1/[controller]")]
+    [ApiController]
+    public class JogosController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<object>>> Obter()
+        {
+            return Ok();
+        }
+
+        [HttpGet("{idJogo:guid}")]
+        public async Task<ActionResult<object>> Obter(Guid idJogo)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<object>> InserirJogo(object jogo)
+        {
+            return Ok();
+        }
+
+        [HttpPut("{idJogo:guid}")]
+        public async Task<ActionResult> AtualizarJogo(Guid idjogo, object jogo)
+        {
+            return Ok();
+        }
+
+        [HttpPatch("{idJogo:guid}/preco{preco:double}")]
+        public async Task<ActionResult> AtualizarJogo(Guid idjogo, double preco)
+        {
+            return Ok();
+        }
+
+        [HttpDelete("{idJogo:guid}")]
+        public async Task<ActionResult> AtualizarJogo(Guid idjogo)
+        {
+            return Ok();
+        }
+    }
+}
